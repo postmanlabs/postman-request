803d2c0aa73a1d3f866324a773d8b90f11058e67
SamvelRaja <samvel@getpostman.com>
2017-01-13 17:50:26 +0530

    Fixed the unwanted decoding happening in the url parser

--- tests/test-url-parse.js
+++ tests/test-url-parse.js
@@ -126,12 +126,27 @@ tape('stringify - "a=b&c=d"', function (t) {
   t.end()
 })
 
-tape('stringify - "a=обязательный&c=d"', function (t) {
+tape('stringify Russian - "a=Привет Почтальон"', function (t) {
   var parsed = [
-        { key: 'a', value: 'обязательный' },
-        { key: 'c', value: 'd' }
+        { key: 'a', value: 'Привет Почтальон' },
   ]
-  t.equal(url.stringify(parsed), 'a=%D0%BE%D0%B1%D1%8F%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9&c=d')
+  t.equal(url.stringify(parsed), 'a=%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD')
+  t.end()
+})
+
+tape('stringify Chinese- "a=你好"', function (t) {
+  var parsed = [
+        { key: 'a', value: '你好' },
+  ]
+  t.equal(url.stringify(parsed), 'a=%E4%BD%A0%E5%A5%BD')
+  t.end()
+})
+
+tape('stringify Japanese- "a=ハローポストマン"', function (t) {
+  var parsed = [
+        { key: 'a', value: 'ハローポストマン' },
+  ]
+  t.equal(url.stringify(parsed), 'a=%E3%83%8F%E3%83%AD%E3%83%BC%E3%83%9D%E3%82%B9%E3%83%88%E3%83%9E%E3%83%B3')
   t.end()
 })
 
@@ -232,6 +247,14 @@ tape('stringify - "a=b&&c=d"', function (t) {
         { key: 'c', value: 'd' }
   ]
   t.equal(url.stringify(parsed), 'a=b&&c=d')
+  t.end()
+})
+
+tape('stringify - "email=foo%20bar-xyz@gmail.com"', function (t) {
+  var parsed = [
+        { key: 'email', value: 'foo%20bar-xyz@gmail.com' }
+  ]
+  t.equal(url.stringify(parsed), 'email=foo%20bar-xyz@gmail.com')
   t.end()
 })
 
