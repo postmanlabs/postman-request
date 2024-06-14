5c5eb1684ee7fec8276ef62e8b3474c44c7395ee
Parth Verma <parth.verma@postman.com>
2024-06-13 22:02:11 -0700

    Converted ts to js to avoid unnecessary hacks to appease the compiler

--- lib/autohttp/index.ts
+++
@@ -1,8 +0,0 @@
-import { AutoHttp2Agent, globalAgent } from './agent'
-import { request } from './request'
-
-export default {
-  Agent: AutoHttp2Agent,
-  request,
-  globalAgent
-}
--- lib/http2/index.ts
+++
@@ -1,8 +0,0 @@
-import { Http2Agent, globalAgent } from './http2Agent'
-import { request } from './request'
-
-export default {
-  Agent: Http2Agent,
-  request,
-  globalAgent
-}
