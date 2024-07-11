a4bcf3d2351caba09c7de61ad9c36ee8db691349
Parth Verma <parth.verma@postman.com>
2024-07-11 15:54:32 -0700

    Fixed failing tests

--- tests/test-tunnel.js
+++ tests/test-tunnel.js
@@ -485,8 +485,8 @@ function addTests () {
   }, [
     'http connect to localhost:' + ss2.port,
     // it should bubble up the key mismatch error
-    process.versions.node.split('.')[0] < 18 ?'err error:05800074:x509 certificate routines::key values mismatch' :
-      'err error:05800074:x509 certificate routines::key values mismatch'
+    process.versions.node.split('.')[0] < 18 ? 'err error:05800074:x509 certificate routines::key values mismatch'
+      : 'err error:05800074:x509 certificate routines::key values mismatch'
   ])
 }
 
